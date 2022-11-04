12e26281da496d5e22a578e2395238532f5decac
Beef Webster <c_goldsmith@yahoo.com>
2022-11-03 20:19:44 -0700

    populated server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const path = require('path');
+
+// handlebars setup
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ });
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
