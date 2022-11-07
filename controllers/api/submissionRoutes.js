85039595d23b88e08ecf2576de4ffa7029245161
Gordon-Magill <gordon.magill@gmail.com>
2022-11-06 17:42:03 -0800

    Added basic route templates for submissionRoutes and userRoutes

--- controllers/api/submissionRoutes.js
+++ controllers/api/submissionRoutes.js
@@ -4,12 +4,27 @@ const { Submission } = require('../../models/index');
 
 
 // Create new submission
-router.get('/create', async (req, res) => {
+router.post('/create', async (req, res) => {
 
 })
 
 // Get all submissions
 router.get('/', async (req, res) => {
+
+})
+
+// Get one submission
+router.get('/:id', async (req, res) => {
+
+})
+
+// Update one submission
+router.put('/:id', async (req, res) => {
+
+})
+
+// Delete one submission
+router.delete('/:id', async (req, res) => {
 
 })
 
