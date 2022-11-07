3bec5cfdaeaee9d163e954fe3b4c4f58e5268765
Gordon-Magill <gordon.magill@gmail.com>
2022-11-06 16:30:01 -0800

    Added basic template rendering:

--- controllers/staticRoutes.js
+++ controllers/staticRoutes.js
@@ -2,30 +2,25 @@ const router = require('express').Router();
 
 // Homepage / browse
 router.get('/', async (req,res) => {
-
+    res.render('homepage',{})
 })
 
 // Dashboard for posting new content + seeing stats
 router.get('/dashboard', async (req,res) => {
-
+    res.render('dashboard',{})
 })
 
-// Login page
+// Login/signup page
 router.get('/login', async (req,res) => {
-
-})
-
-// Signup page
-router.get('/signup', async (req,res) => {
-
+    res.render('login',{})
 })
 
 // Single submission page
 router.get('/submission/:id', async (req,res) => {
-
+    res.render('submission',{})
 })
 
 // 404
 router.get('/404', async (req,res) => {
-
+    res.render('404',{})
 })
