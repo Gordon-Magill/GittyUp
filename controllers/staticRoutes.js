1f7363bfae4bfa7bb0a21cebcb4e17e8a857e7a3
Gordon-Magill <gordon.magill@gmail.com>
2022-11-06 16:25:56 -0800

    Added template static routes

--- controllers/staticRoutes.js
+++ controllers/staticRoutes.js
@@ -1,2 +1,26 @@
 const router = require('express').Router();
 
+// Homepage / browse
+router.get('/', async (req,res) => {
+
+})
+
+// Login page
+router.get('/login', async (req,res) => {
+
+})
+
+// Signup page
+router.get('/signup', async (req,res) => {
+
+})
+
+// Single submission page
+router.get('/submission/:id', async (req,res) => {
+
+})
+
+// 404
+router.get('/404', async (req,res) => {
+
+})
