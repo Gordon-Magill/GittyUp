fe0449671c775ebd2d3893ed5e24b45b01a76de2
Gordon-Magill <gordon.magill@gmail.com>
2022-11-06 18:43:45 -0800

    All submissionRoutes should be prototyped

--- controllers/api/submissionRoutes.js
+++ controllers/api/submissionRoutes.js
@@ -20,7 +20,7 @@ router.post("/create", async (req, res) => {
     // Create the new submission
     const newSubmission = await Submission.create(submissionBody);
 
-    // Send back confirmatory info
+    // Send confirmatory info
     res.status(200).json(newSubmission);
   } catch (err) {
     // Log and send the error
@@ -41,7 +41,7 @@ router.get("/", async (req, res) => {
     // Log the results
     console.log("submissions:", submissions);
 
-    // Send the results
+    // Send confirmatory info
     res.status(200).json(submissions);
   } catch (err) {
     // Log and send the error
@@ -66,7 +66,7 @@ router.get("/:id", async (req, res) => {
     // Log the results
     console.log("submission:", submission);
 
-    // Send the results
+    // Send confirmatory info
     res.status(200).json(submission);
   } catch (err) {
     // Log and send the error
@@ -78,6 +78,27 @@ router.get("/:id", async (req, res) => {
 // Update one submission
 router.put("/:id", async (req, res) => {
   try {
+    // Get the updated body content from the request
+    const updatedContent = {
+      name: req.body.name,
+      description: req.body.description,
+      content: req.body.content,
+      date_created: Date.now(),
+      user_id: req.session.userID,
+    };
+
+    // Log the content to be updated
+    console.log(updatedContent);
+
+    // Actually update the submission
+    const updatedSubmission = Submission.update(updatedContent, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // Send confirmatory info
+    res.status(200).json(updatedSubmission);
   } catch (err) {
     // Log and send the error
     console.log(err);
@@ -88,6 +109,15 @@ router.put("/:id", async (req, res) => {
 // Delete one submission
 router.delete("/:id", async (req, res) => {
   try {
+    // Destroy the designated submission
+    const destroyedSubmission = Submission.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // Send confirmatory info
+    res.status(200).json(destroyedSubmission);
   } catch (err) {
     // Log and send the error
     console.log(err);
