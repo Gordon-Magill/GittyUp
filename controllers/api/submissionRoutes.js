27fe549d2fcbb8e7ece534348b7b4e4204124a2a
Gordon-Magill <gordon.magill@gmail.com>
2022-11-06 18:30:53 -0800

    GET requests in submissionRoutes prototyped + formatting

--- controllers/api/submissionRoutes.js
+++ controllers/api/submissionRoutes.js
@@ -1,48 +1,98 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // Pull in model info from parent index file in models to descure depencencies' definitions
-const { Submission } = require('../../models/index');
-
+const { Submission } = require("../../models/index");
 
 // Create new submission
-router.post('/create', async (req, res) => {
-    try {
-        // Get the info needed 
-        const submissionBody = {
-            name: req.body.name,
-            description: req.body.description,
-            content: req.body.content,
-            date_created: Date.now(),
-            user_id: req.session.userID
-        };
-    
-        console.log('submissionBody: ',submissionBody)
-        const newSubmission = Submission.create(submissionBody);
+router.post("/create", async (req, res) => {
+  try {
+    // Get the info needed for the new submission
+    const submissionBody = {
+      name: req.body.name,
+      description: req.body.description,
+      content: req.body.content,
+      date_created: Date.now(),
+      user_id: req.session.userID,
+    };
 
-        res.status(200).json(newSubmission)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
-})
+    // Log the info to be provided to the model
+    console.log("submissionBody: ", submissionBody);
+
+    // Create the new submission
+    const newSubmission = await Submission.create(submissionBody);
+
+    // Send back confirmatory info
+    res.status(200).json(newSubmission);
+  } catch (err) {
+    // Log and send the error
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 // Get all submissions
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    // Get all submissions
+    const allSubmissions = await Submission.findAll();
 
-})
+    // Strip out extra sequelize content
+    const submissions = allSubmissions.map((row) => row.get({ plain: true }));
+
+    // Log the results
+    console.log("submissions:", submissions);
+
+    // Send the results
+    res.status(200).json(submissions);
+  } catch (err) {
+    // Log and send the error
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 // Get one submission
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
+    // Get all submissions
+    const oneSubmission = await Submission.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-})
+    // Strip out extra sequelize content
+    const submission = oneSubmission.map((row) => row.get({ plain: true }));
+
+    // Log the results
+    console.log("submission:", submission);
+
+    // Send the results
+    res.status(200).json(submission);
+  } catch (err) {
+    // Log and send the error
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 // Update one submission
-router.put('/:id', async (req, res) => {
-
-})
+router.put("/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    // Log and send the error
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 // Delete one submission
-router.delete('/:id', async (req, res) => {
-
-})
+router.delete("/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    // Log and send the error
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,30 +1,20 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // Pull in model info from parent index file in models to descure depencencies' definitions
-const { User } = require('../../models/index');
+const { User } = require("../../models/index");
 
 // Create new user
-router.post('/create', async (req, res) => {
-
-})
+router.post("/create", async (req, res) => {});
 
 // Get all users
-router.get('/', async (req, res) => {
-
-})
+router.get("/", async (req, res) => {});
 
 // Get one user
-router.get('/:id', async (req, res) => {
-
-})
+router.get("/:id", async (req, res) => {});
 
 // Update one user
-router.put('/:id', async (req, res) => {
-
-})
+router.put("/:id", async (req, res) => {});
 
 // Delete one user
-router.delete('/:id', async (req, res) => {
-
-})
+router.delete("/:id", async (req, res) => {});
 
 module.exports = router;
