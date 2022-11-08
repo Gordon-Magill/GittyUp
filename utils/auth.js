4f514c18fe5dc0217fd35de656613db5ff23ea1c
Gordon-Magill <gordon.magill@gmail.com>
2022-11-07 19:48:54 -0800

    Fixed session processing on views and auth

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,8 @@
 const withAuth = (req, res, next) => {
     // If the user is not logged in, redirect the request to the login route
-    if (!req.session.logged_in) {
+    if (!req.session.loggedIn) {
         res.redirect('/login');
+        return;
     } else {
         next();
     }
