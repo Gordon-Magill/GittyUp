5fe9c5be97020592c3cfabb0c68e89be8f6c4d5f
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-11 13:55:13 -0700

    Created logo - will modify, and solidifying color scheme + navbar layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./views/**/*.{handlebars,html,js}", "./public/**/*.{html,js}"],
   theme: {
     colors: {
+      'white': 'rgb(255,255,255)',
       'blue': 'rgb(0,255,210)',
       'purple': 'rgb(159,84,165)',
       'pink': 'rgb(235,63,103)',
