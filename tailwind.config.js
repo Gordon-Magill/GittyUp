cb41101b141b3d311538078a7e437eae9ed63e11
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-10 23:28:37 -0700

    Implementing tailwind css for home and nav bar and homepage handlebars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./views/**/*.{handlebars,html,js}", "./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      'blue': 'rgb(0,255,210)',
+      'purple': 'rgb(159,84,165)',
+      'pink': 'rgb(235,63,103)',
+      'orange': 'rgb(244,159,64)',
+      'green': 'rgb(112,247,145)',
+      'yellow': 'rgb(248,238,0)',
+      'gray-dark': 'rgb(13,13,12)',
+      'gray': 'rgb(37,36,35)',
+      'gray-light': 'rgb(126,126,123)'
+    },
     extend: {},
   },
   plugins: [],
