68f861a7e7f5d1107a429d7c54022f2e23cc7f13
Gordon-Magill <gordon.magill@gmail.com>
2022-11-10 18:35:56 -0800

    Adding basic background elements to make animated grid

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,8 +1,29 @@
+// let tl = anime.timeline({});
+
+// tl.add({
+//   targets: ".animatedPopIn",
+//   translateX: [-3000, 0],
+//   delay: anime.stagger(50, { from: "last" }),
+//   duration: 1000,
+// });
+console.log($(".animatedPopIn"));
 
 anime({
-    targets: '.submission',
-    translateX: [-3000, 0],
-    delay: anime.stagger(50,{from:'last'}),
-    duration: 1000,
+  targets: ".animatedPopIn",
+  translateX: [-2000, 0],
+  delay: anime.stagger(15),
+  duration: 2000,
+  // loop: true,
+  // direction: 'alternate',
+  easing: "easeOutElastic",
+}).finished.then(() => {
+  anime({
+    targets: '.animatedPopIn',
+    translateX: [0, 15],
+    delay: anime.stagger(200),
+    duration: 4000,
+    loop: true,
+    direction: "alternate",
+    easing: "easeInOutSine",
   });
-
+});
