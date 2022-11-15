f1dc4f113c1c0bf879833399a3e8c9fca398c2c6
Beef Webster <c_goldsmith@yahoo.com>
2022-11-14 18:52:10 -0800

    further implementation of comments*

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -2,8 +2,11 @@ const router = require("express").Router();
 
 const session = require("express-session");
 const { Comment } = require("../../models/index");
+const { Submission } = require("../../models/index");
+const { update } = require("../../models/User");
 
 router.post("/create", async (req, res) => {
+  console.log("********************\n\n\nComment Creator Called\n\n\n*********************")
     try {
       const commentBody = {
         author: req.session.username,
@@ -12,7 +15,7 @@ router.post("/create", async (req, res) => {
         author_id: req.session.userID,
         post_id: req.body.postID,
       };
-  
+      
       console.log("commentBody: ", commentBody);
 
       const newcomment = await Comment.create(commentBody);
@@ -22,6 +25,32 @@ router.post("/create", async (req, res) => {
       console.log(err);
       res.status(400).json(err);
     }
+
+    
   });
+
+  //update Submission.comments_count
+
+router.put("/create", async(req, res) => {
+  console.log("********************\n\n\nComment Count Incrementer Called\n\n\n*********************")
+  try{
+    const updateCounter = {
+
+      //this is probably wrong
+      comment_count: comment_count += 1,
+    };
+
+    Submission.update(updateCounter, {
+      where:{
+        // this may be wrong too
+        id: req.params.id
+      },
+    });
+  } catch (err) {
+    // Log and send the error
+    console.log(err);
+    res.status(400).json(err);
+  }
+})
 
 module.exports = router;
