2f89777eda2e0ded06a1892f9e4b8300e1223c1a
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-11-15 23:23:07 -0700

    Hamburger menu working - removed annoying container

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,21 +1,2 @@
-var nav = document.getElementById('menu');
-var header = document.getElementsByTagName('header')
-
-window.addEventListener('scroll', function() {
-  if (window.scrollY >=400) { // adjust this value based on site structure and header image height
-    nav.classList.add('navbar-sticky');
-    header.classList.add('pt-scroll');
-  } else {
-    nav.classList.remove('navbar-sticky');
-    header.classList.remove('pt-scroll');
-  }
-});
-
-function navToggle() {
-        var btn = document.getElementById('hamBtn');
-        var nav = document.getElementById('menu');
-
-        btn.classList.toggle('open');
-        nav.classList.toggle('flex');
-        nav.classList.toggle('hidden');
-    }
+hamMenu.classList.remove("block");
+hamMenu.classList.add("hidden");
